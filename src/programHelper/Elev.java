2bbdfa449b875bf70aaa7c9e24aa16c5cba5042a
findl <findl@DESKTOP-FTJA2KE>
2018-06-10 14:05:35 +0200

    aha

--- src/programHelper/Elev.java
+++ src/programHelper/Elev.java
@@ -1,5 +1,48 @@
 package programHelper;
 
-public class Elev {
+import java.util.ArrayList;
 
+public class Elev {
+	private String firstname;
+	private String surname;
+	private String gender;
+	private ArrayList<String> history;
+	
+	public Elev (String first, String second, String gender) {
+		this.firstname = first;
+		this.surname = second;
+		this.gender = gender.toLowerCase();
+		history = new ArrayList<>();
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public ArrayList<String> getHistory() {
+		return history;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	
+	public String toString() {
+		return firstname +" " + surname;
+	}
 }
